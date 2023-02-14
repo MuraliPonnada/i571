7e257b072f825e7739af39377a60f5909cdada43
MuraliPonnada <mponnad1@binghamton.edu>
2023-02-13 19:26:24 -0500

    updated .sh files

--- projects/prj1/submit/prj1-sol/make.sh
+++ projects/prj1/submit/prj1-sol/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #sets dir to directory containing this script
-dir='/home/mponnad1/PL/cs571/projects/prj1/submit/prj1-sol'
+dir='/home/mponnad1/projects/i571/projects/prj1/submit/prj1-sol'
 
 #use $dir to access programs in this directory
 #so that this script can be run from any directory.
